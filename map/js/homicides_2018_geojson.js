6c09e2303903c53ae6d7b76dbdc29ff070132287
githamm <khamm@denverpost.com>
2018-01-10 13:03:22 -0700

    Add 2018 homicides

---
+++ map/js/homicides_2018_geojson.js
@@ -0,0 +1,65 @@
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -105.027271,
+          39.716005
+        ]
+      },
+      "properties": {
+        "homicide_date": "1/4/2018",
+        "victim_name": "Joshua Jost",
+        "victim_age": "44",
+        "victim_gender": "Male",
+        "manner_of_death": "Sharp force",
+        "block_address": "000 block of South Hazel Court",
+        "neighborhood": "Barnum",
+        "article_link": "https://www.denverpost.com/2018/01/05/denver-police-investigate-homicide-4/"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -104.977792,
+          39.773424
+        ]
+      },
+      "properties": {
+        "homicide_date": "1/6/2018",
+        "victim_name": "Jose Rufugio Herrera-Cabral",
+        "victim_age": "35",
+        "victim_gender": "Male",
+        "manner_of_death": "Gunshot",
+        "block_address": "3700 block of Chestnut Place",
+        "neighborhood": "Five Points",
+        "article_link": "https://www.denverpost.com/2018/01/05/denver-police-respond-shooting-northeast-downtown/"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -104.977792,
+          39.773424
+        ]
+      },
+      "properties": {
+        "homicide_date": "1/6/2018",
+        "victim_name": "Jose Rufugio Herrera-Cabral, 35, and Guillermo Ornelas",
+        "victim_age": "43",
+        "victim_gender": "Male",
+        "manner_of_death": "Gunshot",
+        "block_address": "3700 block of Chestnut Place",
+        "neighborhood": "Five Points",
+        "article_link": "https://www.denverpost.com/2018/01/05/denver-police-respond-shooting-northeast-downtown/"
+      }
+    }
+  ]
+}
